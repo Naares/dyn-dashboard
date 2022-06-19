630066b22ecc1a2014b0e31fccaf2f4d4dff42aa
Jan Kolář <kolar-jan885@post.cz>
2022-06-19 11:49:17 +0200

    Added library documentation links

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export class Main extends React.Component<IMainState>{
     render(): React.ReactNode {
-        return <div>Hello world</div>;
+        return <div>there will be probably one tab the administration panel will be hidden, for start just
+            hardcode some graphs
+        </div>;
     }
 }
 
